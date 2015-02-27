291c193fb03bfc003ad966082ba2e552703207ac
ganto <ganto@qq.com>
2015-02-27 13:41:30 +0000

    common

--- resources/views/home/partials/sidebar.blade.php
+++ resources/views/home/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
 	<div class="sidebar-module sidebar-module-inset">
-		<h4>Categories</h4>
+		<h1>Categories</h1>
 		<ol class="list-unstyled">
 			@foreach($allCategories as $category)
 				<li>
@@ -12,24 +12,24 @@
 			@endforeach 
 		</ol>
 	</div>
-	<div class="sidebar-module">
-		<h4>Hottest Articles</h4>
+	<div class="sidebar-module sidebar-module-inset">
+		<h1>Hot</h1>
 		<ol class="list-unstyled">
 			@foreach($hottestArticles as $article)
 				<li><a href="/{{ $article->slug }}">{{ $article->title }}</a></li>
 			@endforeach
 		</ol>
 	</div>
-	<div class="sidebar-module">
-		<h4>Newest Articles</h4>
+	<div class="sidebar-module sidebar-module-inset">
+		<h1>New</h1>
 		<ol class="list-unstyled">
 			@foreach($NewestArticles as $article)
 				<li><a href="/{{ $article->slug }}">{{ $article->title }}</a></li>
 			@endforeach
 		</ol>
 	</div>
-	<div class="sidebar-module">
-		<h4>Friends</h4>
+	<div class="sidebar-module sidebar-module-inset">
+		<h1>Friends</h1>
 		<ol class="list-unstyled">
 			<li><a href="#">GitHub</a></li>
 			<li><a href="#">Twitter</a></li>
