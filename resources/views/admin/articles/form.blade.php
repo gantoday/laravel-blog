b75f0a9838302a0f4ebe2c8491bc2498e9c6d5fe
ganto <ganto@qq.com>
2015-03-02 05:27:58 +0000

    update article form

--- resources/views/admin/articles/form.blade.php
+++ resources/views/admin/articles/form.blade.php
@@ -5,7 +5,7 @@
 
 <div class="form-group">
 	{!! Form::label('body', 'Body:') !!}
-	{!! Form::textarea('body', null, ['id' => 'editor', 'class' => 'form-control', 'placeholder' => 'Please Enter some text...',  'style' => 'overflow-x:hidden', 'rows' => '22']) !!}
+	{!! Form::textarea('body', null, ['id' => 'editor', 'wrap' => 'virtual', 'class' => 'form-control', 'placeholder' => 'Please Enter some text...',  'style' => 'overflow-x:hidden', 'rows' => '22']) !!}
 </div>
 
 <div class="form-group">
