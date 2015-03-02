ebca9c55f81f2a2eaca25d5e5f1b7bb3c3b0c854
ganto <ganto@qq.com>
2015-03-02 07:06:21 +0000

    update duoshuo thread key

--- resources/views/home/articles/show.blade.php
+++ resources/views/home/articles/show.blade.php
@@ -22,7 +22,7 @@
 	<hr>
 
 	<!-- 多说评论框 start -->
-	<div class="ds-thread" data-thread-key="{{ $article->id }}" data-title="{{ $article->title }}" data-url="{{ setting('site_url').$article->slug }}"></div>
+	<div class="ds-thread" data-thread-key="{{ $article->slug }}" data-title="{{ $article->title }}" data-url="{{ setting('site_url').$article->slug }}"></div>
 	<!-- 多说评论框 end -->
 	<!-- 多说公共JS代码 start (一个网页只需插入一次) -->
 	<script type="text/javascript">
